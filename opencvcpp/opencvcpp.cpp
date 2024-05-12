6ee0e2fe652dfa725dbfd9e080d9179d61753f70
ssun9 <ssun9@DESKTOP-80NG4IV>
2024-05-12 22:49:40 +0900

    초기 커밋.

---
+++ opencvcpp/opencvcpp.cpp
@@ -0,0 +1,13 @@
+﻿#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main() {
+	cv::Mat image = cv::imread("example.jpg");
+	if (image.empty()) {
+		std::cout << "이미지를 불러올 수 없습니다." << std::endl;
+		return -1;
+	}
+	cv::imshow("Image", image);
+	cv::waitKey(0);
+	return 0;
+}
